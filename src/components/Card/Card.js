0bc4616e215945ae880824e7cec9acc972f50788
DAC125 <dcapro170_@hotmail.com>
2022-10-05 16:29:23 -0600

    some changes on project titles

--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -1,24 +1,33 @@
 import React from "react";
 import Card from "react-bootstrap/Card";
+import "./Card.css"
 
 const CardProject = (props) => {
-
-//     const handleClick = () => {
-//         location.href = "https://www.google.com/"
-//     }
-// }
+  //     const handleClick = () => {
+  //         location.href = "https://www.google.com/"
+  //     }
+  // }
   return (
     <Card style={{ width: "30%", margin: "20px" }}>
       <Card.Img
-        style={{ width: "286px", height: "180px", borderRadius: "10px"}}
+        style={{ width: "286px", height: "180px", borderRadius: "10px" }}
         variant="top"
         src={props.img}
       />
       <Card.Body>
-        <Card.Title style={{ color: "white", fontSize: "30px", fontWeight: "bold"}}>{props.title}</Card.Title>
-        <Card.Text style={{margin: "10px"}}>{props.text}</Card.Text>
-    
-        <a href={props.projectRepo} className="colorButton">Go to project</a>
+        <div className="card-title">
+          {/* <Card.Title
+            style={{ color: "white", fontSize: "30px", fontWeight: "bold" }}
+          >
+            
+          </Card.Title> */}
+          <h5 style={{ color: "white", fontSize: "30px", fontWeight: "bold" }}>{props.title}</h5>
+        </div>
+        <Card.Text style={{ margin: "10px" }}>{props.text}</Card.Text>
+
+        <a href={props.projectRepo} className="colorButton">
+          Go to project
+        </a>
       </Card.Body>
     </Card>
   );
